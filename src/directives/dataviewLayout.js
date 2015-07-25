c1a35bcb54e7b64427ff516cdda8d6badc09f17c
Michal Dvorak (cen38289) <michadvorak@csas.cz>
2015-07-25 11:29:06 +0200

    Added some ngdoc for dataviewLayout

--- src/directives/dataviewLayout.js
+++ src/directives/dataviewLayout.js
@@ -1,10 +1,40 @@
 "use strict";
 
+/**
+ * @ngdoc service
+ * @name mdvorakDataviewLayout.dataviewLayoutProvider
+ *
+ * @description
+ * Handles registration of view layouts during config phase.
+ */
 module.provider('dataviewLayout', function dataviewLayoutProvider() {
     var provider = this;
 
+    /**
+     * @ngdoc property
+     * @propertyOf mdvorakDataviewLayout.dataviewLayoutProvider
+     * @name typeAttribute
+     * @type {string}
+     *
+     * @description
+     * Name of the scope variable, that holds the view type. Default is `$viewType`.
+     */
+    provider.typeAttribute = '$viewType';
+
+    // TODO rename group to 'layout'
     provider.groups = {};
 
+    /**
+     * @ngdoc method
+     * @methodOf mdvorakDataviewLayout.dataviewLayoutProvider
+     * @name group
+     *
+     * @param {string=} group Name of the registered group. If omitted, `default` group is used.
+     * @param {Object} config Configuration object. Must have set either `template` or `templateUrl` property.
+     *
+     * @description
+     * Registers configuration for the given group. If the group already exists, it is overwritten.
+     */
     provider.group = function groupFn(group, config) {
         if (arguments.length < 2) {
             config = group;
@@ -18,6 +48,18 @@ module.provider('dataviewLayout', function dataviewLayoutProvider() {
         provider.groups[group] = config;
     };
 
+    /**
+     * @ngdoc service
+     * @name mdvorakDataviewLayout.dataviewLayout
+     *
+     * @description
+     * Provides access to registered view layouts by their group.
+     * To access them, use
+     * {@link mdvorakDataviewLayout.dataviewLayout#methods_$template dataviewLayout.$template(group, viewType)}
+     * method.
+     *
+     * All templates are compiled on demand and linking function is cached.
+     */
     this.$get = function dataviewLayoutFactory($sce, $templateRequest, $compile) {
         var groups = {};
 
@@ -56,23 +98,63 @@ module.provider('dataviewLayout', function dataviewLayoutProvider() {
             }); // forEach config
         }); // forEach groups
 
-        // Accessor function
-        groups.$$template = function $$template(group, viewType) {
+        /**
+         * @ngdoc method
+         * @methodOf mdvorakDataviewLayout.dataviewLayoutProvider
+         * @name $template
+         *
+         * @param {string} group Name of the registered group. If empty or group does not exist, `default` is used.
+         * @param {string} viewType Type of the view the template is for.
+         * @return {Object} Object representing given view. It has defined `link(scope, cloneFn, options)` function,
+         *                  same as returned by `$compile` factory.
+         *
+         * @description
+         * Returns an object representing the given view. If group is not found and default view is not defined,
+         * `undefined` is returned.
+         */
+        groups.$template = function $template(group, viewType) {
             var templates = groups[group] || groups.default;
             return templates && (templates[viewType] || templates.default);
         };
+
+        /**
+         * @ngdoc property
+         * @propertyOf mdvorakDataviewLayout.dataviewLayout
+         * @name $typeAttribute
+         * @type {string}
+         *
+         * @description
+         * Name of the scope variable, that holds the view type. Default is `$viewType`.
+         * This is read-only property.
+         */
+        Object.defineProperty(groups, '$typeAttribute', {
+            enumerable: false,
+            writable: false,
+            value: provider.typeAttribute
+        });
 
         return groups;
     };
 });
 
+/**
+ * @ngdoc directive
+ * @name mdvorakDataviewLayout.dataviewLayout
+ * @restrict EAC
+ * @priority 0
+ *
+ * @param {template=} type Optional. Name of the group for the view. If not set, `default` will be used.
+ *
+ * @description
+ * TODO
+ */
 module.directive('dataviewLayout', function dataviewLayoutDirective($compile, dataviewLayout) {
     return {
         restrict: 'EAC',
         transclude: true,
         link: function dataviewLayoutLink(scope, element, attrs, ctrl, $transclude) {
             var newScope = scope.$new();
-            var template = dataviewLayout.$$template(attrs.type, scope.$viewType);
+            var template = dataviewLayout.$template(attrs.type, scope[dataviewLayout.$typeAttribute]);
 
             if (template) {
                 // Link, let transclude directive handle it
